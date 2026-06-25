cbeaffc25a15dd3b710cc4f25c2c50d43173c03d
sriyaT <tsriya6@gmail.com>
2026-06-25 17:22:37 +0530

    added health check route

--- auth-backend/server/server.js
+++ auth-backend/server/server.js
@@ -14,3 +14,9 @@ app.use("/api/auth", authRoutes);
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "AuthFlow Backend Running 🚀",
+  });
+});
