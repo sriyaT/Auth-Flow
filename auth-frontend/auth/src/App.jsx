2cbdb674f82a5ac6ef278821e08ced702a554efc
sriyaT <tsriya6@gmail.com>
2026-06-26 07:06:02 +0530

    Fix case-sensitive AppRoutes import

--- auth-frontend/auth/src/App.jsx
+++ auth-frontend/auth/src/App.jsx
@@ -1,4 +1,4 @@
-import AppRoutes from "./routes/AppROutes";
+import AppRoutes from "./routes/AppRoutes";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import "./App.css";
