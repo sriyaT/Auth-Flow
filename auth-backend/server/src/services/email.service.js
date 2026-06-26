3f88f1ca7ead036d9dde93fd1ab181f6b3181993
sriyaT <tsriya6@gmail.com>
2026-06-26 21:50:26 +0530

    production test smtp

--- auth-backend/server/src/services/email.service.js
+++ auth-backend/server/src/services/email.service.js
@@ -3,7 +3,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // true only for port 465
+  secure: false,
+  family: 4, // 👈 Force IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
