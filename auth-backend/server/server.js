a2001d7b36de6fceae8525decff11cb04c29e051
sriyaT <tsriya6@gmail.com>
2026-06-26 07:45:37 +0530

    Fix CORS for Vercel frontend

--- auth-backend/server/server.js
+++ auth-backend/server/server.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
 });
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://auth-flow-l9d8zg8h9-sriyats-projects.vercel.app",
+  "https://auth-flow-five-iota.vercel.app",
 ];
 
 app.use(
