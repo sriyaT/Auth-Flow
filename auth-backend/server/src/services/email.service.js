7b8ff20f562d89df440fd375f3a16cca8339e708
sriyaT <tsriya6@gmail.com>
2026-06-30 18:46:48 +0530

    debug email service - added logs

--- auth-backend/server/src/services/email.service.js
+++ auth-backend/server/src/services/email.service.js
@@ -3,18 +3,26 @@ const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ from, to, subject, text }) => {
-  return await resend.emails.send({
-    from,
-    to,
-    subject,
-    html: `
-      <div style="font-family:sans-serif">
-        <p>${text}</p>
-      </div>
-    `,
-  });
+  try {
+    const response = await resend.emails.send({
+      from,
+      to,
+      subject,
+      html: `
+        <div style="font-family:sans-serif">
+          <p>${text}</p>
+        </div>
+      `,
+    });
+
+    console.log("Resend response:", response);
+
+    return response;
+  } catch (err) {
+    console.error("Resend Error:");
+    console.error(err);
+    throw err;
+  }
 };
 
-module.exports = {
-  sendEmail,
-};
+module.exports = { sendEmail };
